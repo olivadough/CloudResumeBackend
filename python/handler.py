011723dcc616f7c619e5607037a065b37555d49c
olivadough <oliver9324@gmail.com>
2021-03-26 14:41:30 -0700

    changed lambda and install boto

--- python/handler.py
+++ python/handler.py
@@ -1,2 +1,25 @@
-def handler(event, context):
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+def placeholder(event, context):
             return {'body': 'Hello World! #2', 'statusCode': 200}
+
+def handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('NumberOfVisitors')
+    response = table.update_item(
+        Key={
+            'Name': Count,
+            'Type': Number 
+        },
+        UpdateExpression="SET Count = Count + :val",
+        ExpressionAttributeValues={
+            ':val': 1
+        },
+        ReturnValues="Count"
+    )
+
+    return {
+      'statusCode': 200,
+      'body': response
+    }
